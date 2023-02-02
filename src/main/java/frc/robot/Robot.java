b688a2be860054f63a7811c4797bc146e4e0fb16
Peter Pena <programmeruser517@gmail.com>
2023-02-02 17:50:52 -0500

    called shuffleboard inti to actually bring in methods, mainly to setup debugging for today's robot testing branch (soon to be created)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -105,6 +105,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     m_vroomVroom.brakeMode();
+    m_shuffleName.init();
   }
 
   /** This function is called periodically during operator control. */
