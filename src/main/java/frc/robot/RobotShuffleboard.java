f6c7fdaa194b47f40d3ac83ed10776570e428a62
Peter Pena <programmeruser517@gmail.com>
2023-02-06 20:16:08 -0500

    more changes

--- src/main/java/frc/robot/RobotShuffleboard.java
+++ src/main/java/frc/robot/RobotShuffleboard.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.GenericEntry;
+//import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -12,19 +14,33 @@ public class RobotShuffleboard {
     Shuffleboard m_shuffleboard;
     boolean m_isLevel;
 
-    private NetworkTableEntry m_isLevelEntry;
+    private GenericEntry m_isLevelEntry;
 
     public RobotShuffleboard() {
         m_driverTab = Shuffleboard.getTab("Driver Tab");
     }
 
     public void init() {
-        //TODO: call drivetrainShuffleboardConfig() method, to be created
+        drivetrainShuffleboardConfig();
     }
 
     public void periodic(boolean isBotLevel) {
         SmartDashboard.putBoolean("is Level", isBotLevel);
     }
 
-    //around line 170 in 2022 code
+    private void setIsLevelEntryThing() {
+        m_isLevel = m_isLevelEntry.getBoolean(false);
+    }
+
+    public boolean getIsLevelThing(){
+        setIsLevelEntryThing();
+        return m_isLevel;
+    }
+
+    public void drivetrainShuffleboardConfig() {
+        Shuffleboard.selectTab("Driver Tab");
+        m_isLevelEntry = m_driverTab.addPersistent("is Level?", false)
+                                    .withWidget(BuiltInWidgets.kBooleanBox)
+                                    .getEntry();
+    }
 }
