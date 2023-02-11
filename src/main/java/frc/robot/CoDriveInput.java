0420d8914cb6e426dbd841601b618a61f799e278
EmmaOverbeek <emmaroverbeek@gmail.com>
2023-02-11 12:00:30 -0500

    Created the claw and shoulder classes and implemented them into Robot.java.

--- src/main/java/frc/robot/CoDriveInput.java
+++ src/main/java/frc/robot/CoDriveInput.java
@@ -1,11 +1,16 @@
 package frc.robot;
 
+import frc.robot.Claw.ClawState;
+import frc.robot.Shoulder.ShoulderState;
+
 /**
  * This encapsulates the inputs for elevator and arm (possible claw later)
  */
 public class CoDriveInput {
     public double m_elevatorPos;
     public double m_armPos;
+    public ClawState m_clawPos;
+    public ShoulderState m_shoulderPos;
 
     /**
      * Default constructor which sets zero values
@@ -13,16 +18,44 @@ public class CoDriveInput {
     public CoDriveInput() {
         m_elevatorPos = 0;
         m_armPos = 0;
-    }
+        //TODO: might want to switch default value to closed in the future.
+        m_clawPos = ClawState.kOpen;
+        m_shoulderPos = ShoulderState.kDown;
+    }  
 
     /**
-     * Constructor that sets the speed and turn
+     * Constructor that sets the elevator and arm positions.
      * @param elevatorPos
      * @param armPos
      */
     public CoDriveInput(double elevatorPos, double armPos) {
         m_elevatorPos = elevatorPos;
         m_armPos = armPos;
+    }
+
+    /**
+     * Constructor that sets the elevator, arm, and claw positions.
+     * @param elevatorPos
+     * @param armPos
+     * @param clawPos
+     */
+    public CoDriveInput(double elevatorPos, double armPos, ClawState clawPos) {
+        m_elevatorPos = elevatorPos;
+        m_armPos = armPos;
+        m_clawPos = clawPos;
+    }
+
+     /**
+     * Constructor that sets the elevator, arm, claw, and shoulder positions.
+     * @param elevatorPos
+     * @param armPos
+     * @param clawPos
+     */
+    public CoDriveInput(double elevatorPos, double armPos, ClawState clawPos, ShoulderState shoulderPos) {
+        m_elevatorPos = elevatorPos;
+        m_armPos = armPos;
+        m_clawPos = clawPos;
+        m_shoulderPos = shoulderPos;
     }
 }
 
