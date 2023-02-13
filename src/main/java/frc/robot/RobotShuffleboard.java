10a5787f140ee4e605ebce5a255c18349fefc695
Peter Pena <programmeruser517@gmail.com>
2023-02-12 21:00:12 -0500

    cleaned up some files, minor changes that were tested with success

--- src/main/java/frc/robot/RobotShuffleboard.java
+++ src/main/java/frc/robot/RobotShuffleboard.java
@@ -10,12 +10,8 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
  */
 public class RobotShuffleboard {
 
-    //member variables instantiated for object use
+    //member variables instantiated for tab object use
     ShuffleboardTab m_driverTab;
-    //more shuffle board member variables; TODO: BOTH CURRENTLY HERE MIGHT BE UNUSED, SHOULD BE TESTED
-    Shuffleboard m_shuffleboard;
-    Drivetrain m_drivetrain;
-    boolean m_isLevel;
 
     //Table label entries are created here
     private GenericEntry m_isLevelEntry;
@@ -27,7 +23,9 @@ public class RobotShuffleboard {
         m_driverTab = Shuffleboard.getTab("Driver Tab");
     }
 
-    //Init method to configure each of the shuffleboard widgets
+    /**
+     * Init method to configure each of the shuffleboard widgets
+     */
     public void init() {
         shuffleboardConfig();
     }
@@ -36,25 +34,12 @@ public class RobotShuffleboard {
      * Periodic method of Shuffleboard
      * mainly for updating DriverTab box values
      * @param isBotLevel is a boolean passed in for the isLevel widget for updating
+     * @SmartDashboardComments with SmartDashboard outputs are for use if the Entry tabs do not properly update, but they have no use at the moment (as the tabs do properly update)
      */
     public void periodic(boolean isBotLevel) {
         //SmartDashboard.putBoolean("is Level", isBotLevel);
         m_isLevelEntry.setBoolean(isBotLevel);
     }
-
-/**
- * No need for this code to be used at the moment.
- * Code pseudo borrowed from 2022 Robot code, not useful, but used in initial implementation for debugging Shuffleboard.
- * 
-    private void setIsLevelEntryThing() {
-        m_isLevel = m_isLevelEntry.getBoolean(false);
-    }
-
-    public boolean getIsLevelThing(){
-        setIsLevelEntryThing();
-        return m_isLevel;
-    }
-*/
 
     /**
      * Method for setting widgets up with entry member variables updated to reflect widget entry values; returns and takes nothing
