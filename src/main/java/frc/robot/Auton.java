4d474cd3fc0451183966831ca786c3b9790692a2
Peter Pena <programmeruser517@gmail.com>
2023-02-13 11:06:50 -0500

    revised many auton logic errors and completed working code for 0 object auton; it builds, but needs to be tested for issue resolution

--- src/main/java/frc/robot/Auton.java
+++ src/main/java/frc/robot/Auton.java
@@ -7,10 +7,16 @@ public class Auton {
     //integer and String, respectively, to store what step (by number, 1-? steps) and what path we are on (0 or 1 object)
     private int m_step;
     private String m_path = "";
-    //initally sets AutonPath to 0 object, creates member variable for currentPath (auton)
+    //default sets AutonPath to 0 object, creates member variable for currentPath (auton)
     private double m_currentAutonPath = RobotMap.RobotShuffleboardConstants.DEFAULT_AUTON_PATH;
-    //Boolean for showing whether auton is started and is set to false otherwise and after
-    boolean m_autonStartOut = true;
+    //boolean for showing whether auton is started and is set to false otherwise and after
+    boolean m_autonStartOut = false;
+    //boolean for running out of class method, set to false by default
+    boolean run = false;
+    //boolean for autoLeveling function implementation
+    boolean toRunAutoLevelOrNotToRun = false;
+    //for use in counting cycles of periodic for wait time
+    private int periodicTicCounter = 0;
 
 
     /**
@@ -32,7 +38,9 @@ public class Auton {
         //initializes elements of robot for the Auton specifically
         //m_currentAutonPath = m_robotShuffleboard.getAutonPath();
         m_step = 1;
+        m_autonStartOut = true;
         selectPath();
+        //TODO: set path from shuffleboard choice here
     }
 
     /**
@@ -40,7 +48,7 @@ public class Auton {
      * @return whether auton is running
      */
     public boolean isRunning() {
-        if (m_path != ""){
+        if (m_path != "" || m_autonStartOut){
             return true;
         }
         else {
@@ -70,10 +78,12 @@ public class Auton {
     }
 
     /**
-     * Method that contains entirety of Auton code and what to do in steps, updates steps to shuffleboard and bot
-     * Returns nothing, takes no parameters.
+     * Method that contains entirety of Auton code and what to do in steps, also updates steps to shuffleboard and bot
+     * @param drivePos is a DriveEncoderPos object that is passed in to periodically grab the current position
+     * @param isBotLevelBack is a boolean input for passed in from Robot to see if the bot is level; mostly for charging station
+     * @return DriveInput object that can control drivetrain outside of drivetrain class via position encoders
      */
-    public DriveInput periodic(DriveEncoderPos drivePos) {
+    public DriveInput periodic(DriveEncoderPos drivePos, boolean isBotLevelBack) {
         DriveInput driveInput = new DriveInput(0, 0, Gear.kLowGear);
         if (m_autonStartOut){
             System.out.println("AUTON STARTED");
@@ -84,19 +94,51 @@ public class Auton {
         if (m_path == "zero"){
             if (m_step == 0) {
                 m_step += 1;
-                System.out.println("Internal auton configuration error detected: non-fatal error. AUTON START UP CONTINUING");
+                System.out.println("Internal auton configuration error detected: non-fatal error. AUTON START UP CONTINUING BUT NOTED");
             }
             //FOR THE STEPS, WE WANT TO: 
             //1. move forward out of the community, 
             //2. move backward enough to start 
             //3. auto level function
             else if (m_step == 1) {
+                //TODO: adjust encoder ticks to reflect 140 inches out, currently set for 2 wheel revolutions
                 if (drivePos.m_leftLeaderPos >= 4096 && drivePos.m_rightLeaderPos >= 4096) {
                     //speed and turn are already set to 0 in driveInput
                     m_step += 1;
                 }
-                driveInput.m_speed = 0.4;
-                driveInput.m_turnSpeed = 0;
+                else {
+                    driveInput.m_speed = 0.4;
+                    driveInput.m_turnSpeed = 0;
+                }
+            }
+            else if (m_step == 2) {
+                if (!isBotLevelBack){
+                   //speed and turn are already set to 0 in driveInput
+                   m_step += 1;
+                }
+                else {
+                    //slightly slower for backing up
+                    driveInput.m_speed = 0.3;
+                    driveInput.m_turnSpeed = 0;
+                }
+            }
+            else if (m_step == 3) {
+                //200 20ms time stamps is over 4 total seconds of wait time
+                if (periodicTicCounter >= 200) {
+                    //do the autoLevel thing
+                    toRunAutoLevelOrNotToRun = true;
+                    if (isBotLevelBack) {
+                        toRunAutoLevelOrNotToRun = false;
+                        m_step += 1;
+                    }
+                }
+                else {
+                    periodicTicCounter++;
+                }
+            }
+            else if (m_step == 4){
+                //TODO: end Auton, instantiate some variables, alert user
+                System.out.println("AUTON NOW ENDED");
             }
         }
         
