263a1cb663e6e222d66106767c88b3f57435bb9b
EmmaOverbeek <emmaroverbeek@gmail.com>
2023-03-11 08:20:58 -0500

    Add crawl and change default auton

--- src/main/java/frc/robot/PilotController.java
+++ src/main/java/frc/robot/PilotController.java
@@ -23,8 +23,12 @@ public class PilotController {
     public DriveInput getDriverInput() {
         DriveInput driverInput = new DriveInput();
         //arcade drive controls
-        driverInput.m_speed = (m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis());
-
+        if (m_controller.getBackButton()) {
+            driverInput.m_speed = -0.07; // crawl speed
+        }
+        else {
+            driverInput.m_speed = (m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis());
+        }
         //Adjusting for a deadband to compensate for controller stick drift.
         Double turnInput = m_controller.getLeftX();
         Double squaredTurnInput = turnInput * turnInput;
